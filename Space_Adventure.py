e20d426dd7ae745d41adad154de8dd590d069c99
Ian <surouxx@gmail.com>
2016-03-01 14:24:52 -0500

    The first part of the game

---
+++ Space_Adventure.py
@@ -0,0 +1,26 @@
+
+
+print "Welcome to my space adventure!\n\n"
+name = raw_input("What is your name cadet?!").title
+
+print "Your vision is fuzzy as you wake up from your extreemly long cryochamber slumber."
+
+print "You hear a fuzzy sound of the alarm wailing in your ears as you cough up the cryo fluid"
+
+print "you flash back to a memory of the onboard doctor explaining why the fluid is in your lungs
+	as you remember its to not give you freezer burn"
+
+print "your vision returns and you see two doors, you barely make out the words, Engineering and Bridge"
+
+ans = raw_input('Go to Engineering or the Bridge')
+
+if ans == 'Enginnering'
+
+	print '''There is an old creak as the soor slides open barely for you and you have to push it the rest of the way.
+	You walk down a vaguely lit hallway and you get to the engineering room where there is this strange creature
+	hunched in a corner, looks at you and runs
+	You are stunned by fear and you see the door controls to your left:'''
+
+ans = raw_input('Lock the blast door or enter cautiously')
+
+if ans == 'Lock' 
---
+++ randomize_code.py
@@ -0,0 +1,11 @@
+import random 
+
+monster = random.random()
+print monster 
+
+if monster > 0.5:
+	will_mon_appear = True 
+
+else:
+	will_mon_appear = False
+print "you move on"
